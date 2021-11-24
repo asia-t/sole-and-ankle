d586fb78b0c249d7ccb669c8af512dccf31b7dd1
Joanna Derks <joanna.derks@schibsted.pl>
2021-11-24 15:03:12 +0100

    ex. 1: super header

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -13,29 +13,37 @@ const SuperHeader = () => {
       <MarketingMessage>
         Free shipping on domestic orders over $75!
       </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
+        <SearchInput />
+        <HelpLink href="/help">Help</HelpLink>
+        <UnstyledButton>
+            <Icon id="shopping-bag" strokeWidth={1} />
+        </UnstyledButton>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
+  display: flex;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  padding-left: 32px;
+  padding-right: 32px;
+  height: 40px;
+  align-items: center;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
   color: inherit;
   text-decoration: none;
   outline-offset: 2px;
+  margin-left: 24px;
+  margin-right: 24px;
 
   &:not(:focus-visible) {
     outline: none;
